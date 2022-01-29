90787ddaf887290f850f2cf2d0bf622d68b14562
Will Bishop <will.bishop9427@gmail.com>
2022-01-29 13:00:16 +0000

    Added PCA code and requirements

---
+++ PCA.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+
+def pca(X, dim):
+    X = X - np.sum(X, axis=0) / X.shape[0]
+    U, S, Vt = np.linalg.svd(X)
+    return np.matmul(U[:, :dim], np.diag(S)[:dim, :dim])
+
+
+
+
+
+
+if __name__ == '__main__':    
+    pass
+    
